37006dd32a67144642ff26fcff6dfdbce4075cd9
Subendhiran <134794710+Subendhiran@users.noreply.github.com>
2025-12-11 12:07:19 +0530

    Fix typo in health method return message

--- src/main/java/com/fazon/simplybytespringboot/controller/TestingController.java
+++ src/main/java/com/fazon/simplybytespringboot/controller/TestingController.java
@@ -10,7 +10,7 @@ public class TestingController {
 
     @GetMapping
     public String health() {
-    return "Hello Everyone, Pipeline change is done!!";
+    return "Hello Everyone, Pipeline change is done!";
     }
 
 }
